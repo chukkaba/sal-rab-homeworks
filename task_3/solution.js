826e70111c43a069d0deaaeb9b6fafe2428c99f8
SeleznevaEk <selezneva71514@gmail.com>
2023-03-05 17:44:50 +0300

    Task 3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -3,6 +3,12 @@ function renderCartItem(item) {
 
     // Задание №3.1. Формирование строки корзины
 
+    let itemCountText = ""
+
+    let sum = product.count * product.price
+
+    itemCountText = `${product.count} x ${product.price} ₽ = ${sum} ₽`
+
     // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
